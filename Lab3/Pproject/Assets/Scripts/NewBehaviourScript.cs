088af01daf67ddd52a809fb07e6f2164e11bf242
Dan Kalmykov <80004416+Kadan19@users.noreply.github.com>
2021-10-19 11:55:24 +0300

    Add Lab3

---
+++ Lab3/Pproject/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        transform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+    }
+    void OnMouseUp()
+    {
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+}
